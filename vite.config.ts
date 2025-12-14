ab21f7fd7db55dae2153e98c4ca9d33d6d0477c9
Shivam Panseja <Panseja05@gmail.com>
2025-12-14 17:42:18 +0530

    feat: vite config changed for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    optimizeDeps: {
-      exclude: ['lucide-react'],
-    },
-    server: {
-      port: parseInt(env.PORT) || 3000,
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
 });
